e303ae4089777d0f4b27b5b1fb62c3bb31e78403
victortelles <victor.telles@iteso.mx>
2025-04-01 09:16:47 -0600

    Funcionalidad para bajar recursos (stopwords)

--- utils/text_processing.py
+++ utils/text_processing.py
@@ -3,3 +3,12 @@ import re
 from nltk.corpus import stopwords
 from nltk import ngrams
 from collections import Counter
+
+# Descargar recursos de NLTK necesarios
+def download_nltk_resources():
+    resources = ['stopwords', 'punkt']
+    for resource in resources:
+        try:
+            nltk.data.find(f'tokenizers/{resource}')
+        except LookupError:
+            nltk.download(resource)
